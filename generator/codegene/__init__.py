996576a50079ea4935d0e5ae7b568fa7c100ea3a
technosvitman <technosvitman@gmail.com>
2021-02-23 08:58:40 +0100

    split generator

--- generator/codegene/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .CodeGenerator import CodeGenerator
-from .Source import Source
-from .Header import Header
-from .Plantuml import Plantuml
--- generator/machine/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .StateMachine import StateMachine
-from .State import StateAction
-from .State import State
